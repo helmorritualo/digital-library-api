9280bc494c8736310fa0ece2be4a1f72d00a947e
killer Syak <helmorritualo@gmail.com>
2025-05-31 22:37:21 +0800

    feat(books): add book management

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -1,3 +1,8 @@
 import { db } from "@/config/database";
 import { books } from "@/db/schema/schema";
 import { eq, desc, and, lt, lte,  getTableColumns } from "drizzle-orm";
+
+export const getAllBooks = async () => {
+    const getAllBooks = await db.select().from(books).orderBy(desc(books.createdAt));
+    return getAllBooks;
+};
